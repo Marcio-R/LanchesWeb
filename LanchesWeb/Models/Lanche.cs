e2ccba070da76409ca7774605ce68eb854ab4262
Marcio Ronaldo Alves <66127342+Marcio-R@users.noreply.github.com>
2023-11-20 20:37:05 -0300

    Add project files.

---
+++ LanchesWeb/Models/Lanche.cs
@@ -0,0 +1,17 @@
+﻿namespace LanchesWeb.Models
+{
+    public class Lanche
+    {
+        public int LancheId { get; set; }
+        public string Nome{ get; set; }
+        public string DescricaoCurta{ get; set; }
+        public string DescricaoDetalhada{ get; set; }
+        public decimal Preco{ get; set; }
+        public string ImagemUrl{ get; set; }
+        public decimal ImagemThumbnailUrl{ get; set; }
+        public bool IsLanchePreferido { get; set; }
+        public bool EmEstoque { get; set; }
+        public int CategoriaId { get; set; }
+        public Categoria Categoria { get; set; }
+    }
+}
